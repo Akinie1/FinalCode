7f93529aa3540b6ca2b3142b6f1e6ab7110efd2f
Akinie1 <108593452+Akinie1@users.noreply.github.com>
2023-04-08 11:36:11 +0000

    Add files via upload

---
+++ course.h
@@ -0,0 +1,185 @@
+#ifndef COURSE_H_INCLUDED
+#define COURSE_H_INCLUDED
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Course{
+public:
+        int course_id;
+        string course_name;
+        int school_id;
+        int max_enrollment;
+        unsigned int basescore;
+public:
+        //The method  helps to set the attributes of the object being created
+        void set_course(int cid,string name,int sid, int max_num,int bscore)
+                {course_id = cid; course_name = name; school_id = sid; max_enrollment = max_num; basescore=bscore; }
+        
+        //This method  is  to help cinsert course detiuals into the csv file
+        void create_course(int schoolid){
+        fstream fout;
+        string coursename;
+        int maxenroll;
+        int rawscore;
+
+
+        ifstream inputfile;
+        inputfile.open("Courses.csv", ios::out | ios::app);
+
+        string line = "";
+        int count =0;
+        int coursecode=1000;
+       while(getline(inputfile, line,'\n')) {
+
+             count++;
+        }
+        coursecode+=count;
+        inputfile.close();
+    fout.open("Courses.csv", ios::out | ios::app);
+        cout <<"Kindly enter the following details to create a course for your school\n";
+        cout <<"coursename:";
+        getline(cin,coursename);
+        cout <<"Maximum enrollment:";
+        cin>> maxenroll;
+        cout <<"Basic raw score:";
+        cin>> rawscore;
+
+        fout<<coursecode<<","
+                <<coursename<<","
+                <<schoolid<<","
+                <<maxenroll<<","
+                <<rawscore<<",\n";
+        fout.close();
+        }
+//Display all courses offered by a school
+    void view_courses(Course *courserecord, int schoolcode){
+
+    vector<Course> courses ;
+    ifstream inputfile;
+    inputfile.open("Courses.csv");
+
+    string line = "";
+    getline(inputfile, line);
+    line = "";
+    while(getline(inputfile,line)){
+        int courseId;
+        string coursename;
+        int schoolid;
+        int maxenroll;
+        int basescore;
+        string  tempString =" ";
+        stringstream inputString(line);
+
+        getline(inputString,tempString,',');
+        courseId =atoi(tempString.c_str());
+
+        getline(inputString,coursename,',');
+
+        getline(inputString,tempString,',');
+
+        schoolid =atoi(tempString.c_str());
+
+        //We create an object of the class for each line where the schoolid in the csv is equal to the argiument passed into the function
+        if (schoolid == schoolcode){
+        getline(inputString,tempString,',');
+         maxenroll=atoi(tempString.c_str());
+
+        getline(inputString,tempString,',');
+        basescore =atoi(tempString.c_str());
+
+        //After every record of Construct, construct course object and add to vector
+        courserecord->set_course(courseId,coursename,schoolid,maxenroll,basescore);
+        courses.push_back(*(courserecord));
+
+
+        line = "";
+        }
+    }
+
+
+    for (auto course : courses){
+            cout<<"Course Id: "<<course.course_id<<endl;
+            cout<<"Name of course: "<<course.course_name<<endl;
+            cout<<"Maximum Number of enrollments : "<<course.max_enrollment<<endl;
+            cout<<"Cutoff Raw Score: "<<course.basescore<<endl;
+            cout<<"\n"<<endl;
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+//Display a specific course for a specific school
+        void view_course(int school_id, int course_id){
+             cout<<"Name of course: "<<course_name<<endl;;
+            cout<<"Maximum Number of enrollments : "<<max_enrollment<<endl;
+            cout<<"The base raw score is: "<<basescore<<endl;
+        }
+
+
+        void retrieve_course(Course*course, int school_id, int coursecode){
+
+    ifstream inputfile;
+    inputfile.open("Courses.csv", ios::out | ios::app);
+    int schoolcode = 2;
+    string line = "";
+    getline(inputfile, line);
+    line = "";
+    while(getline(inputfile,line)){
+        int courseId;
+        string coursename;
+        int schoolid;
+        int maxenroll;
+        int basescore;
+        string  tempString =" ";
+        stringstream inputString(line);
+
+        getline(inputString,tempString,',');
+        courseId =atoi(tempString.c_str());
+
+        getline(inputString,coursename,',');
+
+        getline(inputString,tempString,',');
+
+        schoolid =atoi(tempString.c_str());
+        if (schoolid == school_id && coursecode == courseId){
+        getline(inputString,tempString,',');
+         maxenroll=atoi(tempString.c_str());
+
+        getline(inputString,tempString,',');
+        basescore =atoi(tempString.c_str());
+
+        //After every record of Construct, construct student object and add to vector
+        course->set_course(courseId,coursename,schoolid,maxenroll,basescore);
+
+
+
+        line = "";
+        }
+    }
+
+        }
+private:
+         void delete_course(int course_id){
+
+        }
+       void  update_course(int course_id,int option, string newvalue){
+
+        }
+
+};
+
+#endif // COURSE_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+// some header files
+#include "welcome.h"  
+using namespace std;
+
+
+
+
+int main()
+{
+    
+
+    // The welcome object instantiate the interface of the whole program. 
+    Welcome welcome;
+   
+return 0;
+}
+
